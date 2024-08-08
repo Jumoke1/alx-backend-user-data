1e2fd51acb49792c169cd590c341723949ac0eef
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-08-08 00:51:28 +0000

    index

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods['GET'], strict_slashes =False)
-def testing():
-    """ testing the endpoint"""
+@app_views.route('/unauthorized/', methods['GET'], strict_slashes=False)
+def unauthorised() -> None:
+    """get the /api/v1/unauthorized"""
     abort(401)
