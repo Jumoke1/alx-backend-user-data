32e9cb5f492595c5e4fd06b88cdc141c62d3a7de
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-08-09 17:27:31 +0000

    main

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,12 +13,12 @@ class Auth:
         if path is None:
             return True
 
-        if excluded_paths is None or len(exclude_paths) == 0:
+        if excluded_paths is None or len(excluded_paths) == 0:
             return True
 
         path = path.rstrip('/') + '/'
 
-        if path in excluded_path:
+        if path in excluded_paths:
             return False
 
         return True
