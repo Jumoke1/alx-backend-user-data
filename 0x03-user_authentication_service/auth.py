5f562d284bb8a6a601c279d901e73d583643ac52
“Jumoke1” <kazeemjumoke12@gmail.com>
2024-08-15 13:04:38 +0000

    auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,35 @@
 #!/usr/bin/env python3
 """A module for authentication-related routines"""
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
-def _hash_pasword(password: str) -> bytes:
+def _hash_password(password: str) -> bytes:
     """converting the password to bytes"""
     password_bytes = password.encode('utf-8')
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password_bytes, salt)
     return hashed_password
+
+
+class Auth:
+    """auth class to interact with authentication database"""
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str):
+        """ a method to register a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+            except NoResultFound:
+                pass
+
+    hased_password = self._hash_password(password)
+
+    new_user = self._db.add_user(email=email, hashed_password=hased_password)
+
+    return new_user
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -13,3 +13,13 @@ print(user_1.id)
 
 user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
 print(user_2.id)
+
+
+#!/usr/bin/env python3
+"""
+Main file
+"""
+from auth import _hash_password
+
+print(_hash_password("Hello Holberton"))
+
